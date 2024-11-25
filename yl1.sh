cea8632143db9356b13f370dd91112c29f537af9
Marek Motskevitsus <marek.motskevitsus@voco.ee>
2024-11-25 10:54:06 +0200

    Test Skript

---
+++ yl1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Skript, mis küsib kasutaja käest tema täis_nimi (ees- ja perenimi) ja tervitab teda.
+
+echo -n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi
+echo "Tere tulemast, $eesnimi $perenimi"
+
