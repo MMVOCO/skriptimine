887cc784c7c2eb29405911fee80f434b485622ab
Marek Motskevitsus <marek.motskevitsus@voco.ee>
2024-11-25 11:15:33 +0200

    Remove yl1.sh file

--- yl1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Skript, mis küsib kasutaja käest tema täis_nimi (ees- ja perenimi) ja tervitab teda.
-
-echo -n "Sisesta oma ees- ja perenimi: "
-read eesnimi perenimi
-echo "Tere tulemast, $eesnimi $perenimi"
-
-# Test edit
